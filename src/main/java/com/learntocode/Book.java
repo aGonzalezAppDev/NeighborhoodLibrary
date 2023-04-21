c06dc9dfccfdb31acfd7007596d844ec762e7ac5
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-04-21 15:34:38 -0400

    Third commit

--- src/main/java/com/learntocode/Book.java
+++ src/main/java/com/learntocode/Book.java
@@ -5,12 +5,17 @@ public class Book {
     private String isbn; // International book number
     private String author;
     private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
 
-    // create constructor
-    public Book(int id, String author, String title){
+
+    // create a constructor to create objects in the main
+    public Book(int id, String isbn, String title) {
         this.id = id;
-        this.author = author;
+        this.isbn = isbn;
         this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
     }
 
     // getters and setters for id
@@ -20,6 +25,7 @@ public class Book {
     public void setId(int id) {
         this.id = id;
     }
+
     // getters and setters for author
     public String getAuthor() {
         return author;
@@ -27,6 +33,7 @@ public class Book {
     public void setAuthor(String author) {
         this.author = author;
     }
+
     // getters and setters for title
     public String getTitle() {
         return title;
@@ -34,14 +41,45 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
+
     // getters and setters for isbn
-    public String getIsbn(){
+    public String getIsbn() {
         return isbn;
     }
-    public void setIsbn(){
+    public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
-    
+    // setters & getters for checked out
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+    public void setIsCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    // method for when book is checked out
+    public void checkOut(String name) {
+        this.checkedOutTo = name;
+        this.isCheckedOut = true;
+
+    }
+
+    // method for when book is checked in
+    public void checkIn(){
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+
+
 
 }
--- src/main/java/com/learntocode/NeighborhoodLibrary.java
+++ src/main/java/com/learntocode/NeighborhoodLibrary.java
@@ -1,7 +1,51 @@
 package com.learntocode;
 
-public class NeighborhoodLibrary {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class NeighborhoodLibrary {
+    // making global variables
+    private static Book[] books = new Book[5];
+    private static int numBooks = 5;
+
+    public static void main(String[] args) {
+        // objects of books
+        books[0] = new Book(1, "978-1-945209-05-1", "Little Pony");
+        books[1] = new Book(2, "978-1-945209-05-2", "Dumbo");
+        books[2] = new Book(3, "978-1-945209-05-3", "Art of War");
+        books[3] = new Book(4, "978-1-945209-05-4", "Snow White");
+        books[4] = new Book(5, "978-1-945209-05-4", "Blue Lock");
+
+        Scanner myScanner = new Scanner(System.in);
+
+        // while loop with switch for menu
+        while (true) {
+            System.out.println("Choose an option");
+            System.out.println(" 1 - Show Available Books");
+            System.out.println(" 2 - Show Checked out Books");
+            System.out.println(" 3 - [Exit]");
+            System.out.println("Enter your choice:");
+
+            int choice = myScanner.nextInt();
+            myScanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    // show all books
+                    break;
+                case 2:
+                    // show checked out books
+                    break;
+                case 3:
+                    // exit to close out of screen
+                    return;
+                default:
+                    System.out.println("Choose a valid choice!!");
+                    break;
+            }
+
+        }
     }
+
+
+
 }
